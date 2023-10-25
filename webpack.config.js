37087d66e362fd145aa18c24d84011d5fcb0a4d6
jasperket <jasperkeith1.vallecera@gmail.com>
2023-10-25 10:12:42 +0800

    Finished home page

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   devtool: 'inline-source-map',
   module: {
@@ -19,4 +21,10 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Kutsara at Tinidor',
+      template: './src/index.html',
+    }),
+  ],
 };
