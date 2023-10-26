daa087f2b585c6049323dcc8e23ad65daf2d1bb6
jasperket <jasperkeith1.vallecera@gmail.com>
2023-10-26 19:10:43 +0800

    Added modules for each tab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import './reset.css';
 import './style.css';
-console.log("hello");
+import homeHTML from './js-modules/home';
+
+// const content = document.querySelector("#content");
+// content.innerHTML = homeHTML;
+
+const btnHome = document.querySelector("#home");
+btnHome.addEventListener('click',()=>{
+    content.innerHTML = homeHTML;
+    btnHome.classList.add("selected");
+    btnMenu.classList.remove("selected");
+    btnAbout.classList.remove("selected");
+});
+
+const btnMenu = document.querySelector("#menu");
+btnMenu.addEventListener('click',()=>{
+    content.innerHTML = homeHTML;
+    btnHome.classList.remove("selected");
+    btnMenu.classList.add("selected");
+    btnAbout.classList.remove("selected");
+});
+
+const btnAbout = document.querySelector("#about");
+btnAbout.addEventListener('click',()=>{
+    content.innerHTML = homeHTML;
+    btnHome.classList.remove("selected");
+    btnMenu.classList.remove("selected");
+    btnAbout.classList.add("selected");
+});
---
+++ src/js-modules/home.js
@@ -0,0 +1,11 @@
+const homeHTML = `
+<div class="hero"></div>
+<div class="description home">
+    <div class="desc-text">
+        <h1>Welcome to Kutsara at Tinidor</h1>
+        <p>Where the rich and diverse flavors of Filipino cuisine come to life on your plate. Our restaurant is a culinary journey through the heart and soul of the Philippines, offering an authentic and mouthwatering experience that celebrates the vibrant heritage of Filipino food. From savory adobo to the sweet delights of halo-halo, we invite you to savor the taste of tradition and experience the warmth of Filipino hospitality. Join us for an unforgettable dining adventure that promises to delight your taste buds and transport you to the heart of the Philippines, one delicious dish at a time.</p>
+    </div>
+    <div class="desc-pic adobo"></div>
+</div>
+`;
+export default homeHTML;
